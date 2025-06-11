a7842c771f82c28c81c5ea2892fc64f9497d91ef
ShivaniPandey222 <pandeyshivani6777@gmail.com>
2025-06-11 10:34:57 +0530

    Update UserPresence1ApplicationTests.java

--- src/test/java/com/userPresence1/userPresence1/UserPresence1ApplicationTests.java
+++ src/test/java/com/userPresence1/userPresence1/UserPresence1ApplicationTests.java
@@ -6,9 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserPresence1ApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(UserPresence1ApplicationTests.class);
 	@Test
 	void contextLoads() {
-		System.out.println("shivani testing");
+		log.info("shivani testing - This message should be clearly visible!");
+        log.debug("This is a debug message from shivani's test."); // Will only show if logging level is DEBUG
+        log.warn("This is a warning from shivani's test.");
 	}
 
 }
